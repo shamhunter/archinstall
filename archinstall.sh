c558521801737f2190dbf96d3a047f070dd18a34
shamhunter <nkuzhangshn@gmail.com>
2020-09-11 10:46:48 +0800

    Initsetup

---
+++ archinstall.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# A shell script for people who wants to install archlinux easily.
+# Author  : Shamhunter <nkuzhangshn@gmail.com>
+# Date    : Fri Sep 11 10:38:03 AM CST 2020
+# URL GitHub  : https://github.com/shamhunter/archinstall
+# URL Gitee   : https://gitee.com/shamhunter/archinstall
+# URL Blog    : https://www.onethinker.top/
+# Log file    : /tmp/archinstall.log
+# 
+
